4092faf55dc76ef8291368aacb335eed00de9c93
maximestephan <stephan.maxime@gmail.com>
2025-12-08 15:30:18 +0100

    release 10.1

--- install.bat
+++ install.bat
@@ -1,11 +1,14 @@
 @echo off
 setlocal
 
+set "MINICONDA_DIR=%USERPROFILE%\miniconda3"
+set "APP_DIR=%USERPROFILE%\tiny_decide"
+
 winget install --id Git.Git -e --source winget
-git clone https://github.com/maximestephan/tiny_decide.git "%USERPROFILE%\tiny_decide"
+git clone https://github.com/maximestephan/tiny_decide.git "%APP_DIR%"
 
 winget install --id Anaconda.Miniconda3 -e --source winget
-"%USERPROFILE%\miniconda3\Scripts\conda.exe" env list tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+"%MINICONDA_DIR%\Scripts\conda.exe" tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
 "%USERPROFILE%\miniconda3\Scripts\conda.exe" env list tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 "%USERPROFILE%\miniconda3\Scripts\conda.exe" env list tos accept --override-channels --channel https://repo.anaconda.com/pkgs/msys2
 
