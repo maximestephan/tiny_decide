20549c55bd6bb1893a2b291802ceccfbda63a723
maximestephan <stephan.maxime@gmail.com>
2025-12-08 16:05:59 +0100

    release 10.2

--- install.bat
+++ install.bat
@@ -1,22 +1,20 @@
 @echo off
 setlocal
 
-set "MINICONDA_DIR=%USERPROFILE%\miniconda3"
-set "APP_DIR=%USERPROFILE%\tiny_decide"
 
 winget install --id Git.Git -e --source winget
-git clone https://github.com/maximestephan/tiny_decide.git "%APP_DIR%"
+git clone https://github.com/maximestephan/tiny_decide.git "%USERPROFILE%\tiny_decide"
 
 winget install --id Anaconda.Miniconda3 -e --source winget
-"%MINICONDA_DIR%\Scripts\conda.exe" tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
-"%USERPROFILE%\miniconda3\Scripts\conda.exe" env list tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
-"%USERPROFILE%\miniconda3\Scripts\conda.exe" env list tos accept --override-channels --channel https://repo.anaconda.com/pkgs/msys2
-
+"%USERPROFILE%\miniconda3\Scripts\conda.exe"  tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+"%USERPROFILE%\miniconda3\Scripts\conda.exe" tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+"%USERPROFILE%\miniconda3\Scripts\conda.exe" tos accept --override-channels --channel https://repo.anaconda.com/pkgs/msys2
 "%USERPROFILE%\miniconda3\Scripts\conda.exe" env list | findstr /b /c:"tinydecide" >nul 2>&1
 if errorlevel 1 (
     echo Environment "tinydecide" not found. Creating it now...
-    "%USERPROFILE%\miniconda3\Scripts\conda.exe" env list create -y -n tinydecide python=3.11 rdkit flask pandas requests -c conda-forge
+    "%USERPROFILE%\miniconda3\Scripts\conda.exe" create -y -n tinydecide python=3.11 rdkit flask pandas requests -c conda-forge
 ) else (
     echo Environment tinydecide already exists.
 )
+pause
 
--- run.bat
+++ run.bat
@@ -3,6 +3,6 @@ setlocal
 
 cd /d "%USERPROFILE%\tiny_decide"
 git pull
-call "%USERPROFILE%\miniconda3\Scripts\activate.bat" tinydecide
+"%USERPROFILE%\miniconda3\Scripts\activate.bat" tinydecide
 python -m flask --app main.py run  --port 8080
 start "" "http://127.0.0.1:8080"
